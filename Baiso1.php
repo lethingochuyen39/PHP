f4f0644ca6a365f21680f5258081132f6ffd54a0
DESKTOP-AHSGN44\Ngoc Huyen <huyenle021039@gmail.com>
2022-04-20 18:56:58 +0700

    test

---
+++ Baiso1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai so 1 PHP</title>
+</head>
+<body>
+    <h1>Đây là bài số 1 - PHP </h1>
+    <?php
+    echo "Chào lớp T1.2109.E0"
+    ?>
+    <h2>
+        <?php echo "Đây là text sử dụng H2"?>
+    </h2>
+</body>
+</html>
